4198b03866c0013a22f44c1696ec558355b0ba26
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-07-25 07:28:55 +0000

    feat: pie chart as component

--- src/modules/main/views/Home/Home.jsx
+++ src/modules/main/views/Home/Home.jsx
@@ -11,10 +11,6 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-  PieChart,
-  Pie,
-  Cell,
-  Sector,
 } from "recharts";
 
 import loanService from "../../../loan/loan.service";
@@ -25,6 +21,8 @@ import {
   formatCurrency,
   monthNumberToMonthName,
 } from "../../../../utils";
+
+import TotalByTypesPieChart from "../../../../components/TotalByTypesPieChart";
 
 import { GlobalContext } from "../../../../App.jsx";
 
@@ -53,16 +51,6 @@ const getLoanAmountsChartItems = (items) => {
   });
 };
 
-const getTotalByTypesChartItems = (items) => {
-  return items.map((item) => {
-    return {
-      ...item,
-    };
-  });
-};
-
-const COLORS = ["yellow", "orange", "red", "green"];
-
 const Home = () => {
   const [overviewInfo, setOverviewInfo] = useState(undefined);
   const [overviewInfoLoading, setOverviewInfoLoading] = useState(false);
@@ -73,12 +61,6 @@ const Home = () => {
     useState(false);
   const [totalBorrowedPerMonthError, setTotalBorrowedPerMonthError] =
     useState(undefined);
-
-  const [totalByTypes, setTotalByTypes] = useState([]);
-  const [totalByTypesLoading, setTotalByTypesLoading] = useState(false);
-  const [totalByTypesError, setTotalByTypesError] = useState(undefined);
-
-  const [activeIndex, setActiveIndex] = useState(0);
 
   const ctx = useContext(GlobalContext);
   const navigate = useNavigate();
@@ -120,23 +102,6 @@ const Home = () => {
     setTotalBorrowedPerMonthLoading(false);
   };
 
-  const fetchTotalByTypes = async () => {
-    setTotalByTypesLoading(true);
-
-    try {
-      const [data] = await Promise.all([
-        loanService.getTotalByTypes(),
-        delay(),
-      ]);
-
-      setTotalByTypes(getTotalByTypesChartItems(data));
-    } catch (error) {
-      setTotalByTypesError(getMessageFromAxiosError(error));
-    }
-
-    setTotalByTypesLoading(false);
-  };
-
   useEffect(() => {
     if (!user) {
       return navigate("/");
@@ -144,83 +109,7 @@ const Home = () => {
 
     fetchOverviewInfo();
     fetchTotalBorrowedPerMonth();
-    fetchTotalByTypes();
   }, [navigate, user]);
-
-  const renderActiveShape = (props) => {
-    const RADIAN = Math.PI / 180;
-    const {
-      cx,
-      cy,
-      midAngle,
-      innerRadius,
-      outerRadius,
-      startAngle,
-      endAngle,
-      fill,
-      payload,
-      percent,
-      value,
-    } = props;
-    const sin = Math.sin(-RADIAN * midAngle);
-    const cos = Math.cos(-RADIAN * midAngle);
-    const sx = cx + (outerRadius + 10) * cos;
-    const sy = cy + (outerRadius + 10) * sin;
-    const mx = cx + (outerRadius + 30) * cos;
-    const my = cy + (outerRadius + 30) * sin;
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-    const ey = my;
-    const textAnchor = cos >= 0 ? "start" : "end";
-
-    return (
-      <g>
-        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
-          {payload.name}
-        </text>
-        <Sector
-          cx={cx}
-          cy={cy}
-          innerRadius={innerRadius}
-          outerRadius={outerRadius}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          fill={fill}
-        />
-        <Sector
-          cx={cx}
-          cy={cy}
-          startAngle={startAngle}
-          endAngle={endAngle}
-          innerRadius={outerRadius + 6}
-          outerRadius={outerRadius + 10}
-          fill={fill}
-        />
-        <path
-          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-          stroke={fill}
-          fill="none"
-        />
-        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-        <text
-          x={ex + (cos >= 0 ? 1 : -1) * 12}
-          y={ey}
-          textAnchor={textAnchor}
-          fill="#333"
-        >
-          {`Total ${formatCurrency(value)}`}
-        </text>
-        <text
-          x={ex + (cos >= 0 ? 1 : -1) * 12}
-          y={ey}
-          dy={18}
-          textAnchor={textAnchor}
-          fill="#999"
-        >
-          {`(Porcentaje ${(percent * 100).toFixed(2)}%)`}
-        </text>
-      </g>
-    );
-  };
 
   return (
     <div className="cds--grid">
@@ -365,62 +254,7 @@ const Home = () => {
                   </div>
                 </>
               )}
-            {totalByTypesLoading && (
-              <InlineLoading
-                status="active"
-                description="Cargando..."
-                className={"center-screen"}
-              />
-            )}
-            {totalByTypesError && (
-              <div
-                style={{ marginBottom: "1rem" }}
-                className="screen__notification_container"
-              >
-                <InlineNotification
-                  kind="error"
-                  subtitle={<span>{totalByTypesError}</span>}
-                  title="Uups!"
-                  onClose={() => setTotalByTypesError(undefined)}
-                />
-              </div>
-            )}
-            {!totalByTypesLoading && !totalByTypesError && totalByTypes && (
-              <>
-                <div style={{ marginBottom: "1rem" }}>
-                  <div className="cds--row">
-                    <div className="cds--col">
-                      <p className="screen__label screen__text--center">
-                        Totales por tipo:
-                      </p>
-                      <ResponsiveContainer width="100%" height={300}>
-                        <PieChart width={500} height={300}>
-                          <Pie
-                            activeIndex={activeIndex}
-                            activeShape={renderActiveShape}
-                            data={totalByTypes}
-                            cx="50%"
-                            cy="50%"
-                            labelLine={false}
-                            outerRadius={80}
-                            fill="#8884d8"
-                            dataKey="total"
-                            onClick={(_, index) => setActiveIndex(index)}
-                          >
-                            {totalByTypes.map((entry, index) => (
-                              <Cell
-                                key={`cell-${index}`}
-                                fill={COLORS[index % COLORS.length]}
-                              />
-                            ))}
-                          </Pie>
-                        </PieChart>
-                      </ResponsiveContainer>
-                    </div>
-                  </div>
-                </div>
-              </>
-            )}
+            <TotalByTypesPieChart />
           </div>
         </div>
       </div>
